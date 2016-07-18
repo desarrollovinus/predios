127a56537449f776d5706e1d4e8386fd4aca639d
johncanovinus <john.cano@vinus.com.co>
2016-07-18 10:45:55 -0500

    Tabla de convenciones
    - Modificado el estilo de la tabla de convenciones para los archivos KML

--- application/views/plantillas/tabla-convenciones.php
+++ application/views/plantillas/tabla-convenciones.php
@@ -1,6 +1,8 @@
 <style media="screen">
     body {
         font-family: arial;
+        font-size: 0.8em;
+        color: white;
     }
 
     h1 {
@@ -8,67 +10,95 @@
     }
 
     table {
-       border-collapse:collapse;
-       padding:0;
+        border-collapse:collapse;
+        font-size: 0.8em;
+        padding:0;
     }
 
     table td, th {
-        display: table-cell;
-        text-align: center;
-        width:220px;
-        vertical-align:middle;
+        /*display: table-cell;*/
         height:0;
+        padding: 2.5;
         padding-left: 20px;
-        padding-bottom: 30px;
+        text-align: center;
+        vertical-align:middle;
+        /*width:220px;*/
     }
 
     table td div {
         min-height:100%;
     }
+    img{
+        margin: 0;
+        text-align: right;
+    }
+
+    #convenciones{
+        border: 1px solid white;
+        padding:5px;
+    }
+
+    .cabecera {
+        text-align: center;
+        padding-bottom: 10px;
+        width: 100%;
+    }
+    .cabecera div {
+        /*border: 1px solid black;*/
+        display: inline-block;
+        vertical-align: middle;
+    }
 
 </style>
-<h1>Convención</h1>
-<table>
-    <tr>
+
+<div id="convenciones">
+    <div class="cabecera">
         <div>
-            <th>
-                Estado de las vías:
-            </th>
+            <img src="http://sicc.vinus.com.co/img/Logo_vinus.png" alt="ANI" height="60px" />
         </div>
-        <?php foreach ($estados_via as $estado): ?>
-            <td>
-                <div style="border-bottom: 6px solid #<?php echo $estado->color ?>; ">
-                    <?php echo $estado->nombre; ?>
+
+        <div>
+            <h4>Convenciones de los mapas</h4>
+        </div>
+
+        <div>
+            <img src="http://sicc.vinus.com.co/img/logo_ani.jpg" alt="ANI" height="60px" />
+        </div>
+    </div>
+
+    <table width="100%">
+            <tr>
+                <div>
+                    <th>
+                        Estado de las vías:
+                    </th>
                 </div>
-            </td>
-        <?php endforeach; ?>
-        <td>
-            <img src="http://sicc.vinus.com.co/img/logo_ani.jpg" alt="ANI" height="60px"/>
-        </td>
-    </tr>
-</table>
-<br><br>
-<table>
-    <tr>
-        <th rowspan="2">
-            Estado del proceso:
-        </th>
-        <?php $cont = 0; ?>
-        <?php foreach ($estados_proceso as $estado): ?>
-            <td>
-                <div style="border-bottom: 20px solid #<?php echo $estado->color ?>; ">
-                    <?php echo $estado->estado; ?>
-                </div>
-                <?php if ($cont > 9) {
-                    echo "</tr><tr>";
-                    $cont = 0;
-                }
-                $cont++;
-                 ?>
-            </td>
-        <?php endforeach; ?>
-        <td>
-            <img src="http://sicc.vinus.com.co/img/Logo_vinus.png" alt="ANI" height="60px"/>
-        </td>
-    </tr>
-</table>
+                <?php foreach ($estados_via as $estado): ?>
+                    <td>
+                        <div style="border-bottom: 3px solid #<?php echo $estado->color ?>; ">
+                            <?php echo $estado->nombre; ?>
+                        </div>
+                    </td>
+                <?php endforeach; ?>
+            </tr>
+        <tr>
+            <th rowspan="2">
+                Estado del proceso:
+            </th>
+            <?php $cont = 0; ?>
+            <?php foreach ($estados_proceso as $estado): ?>
+                <td>
+                    <div style="border-bottom: 20px solid #<?php echo $estado->color ?>; ">
+                        <?php echo $estado->estado; ?>
+                    </div>
+                    <?php if ($cont > 9) {
+                        echo "</tr><tr>";
+                        $cont = 0;
+                    }
+                    $cont++;
+                     ?>
+                </td>
+            <?php endforeach; ?>
+        </tr>
+    </table>
+</div>
